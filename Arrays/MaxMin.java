6da6b8eec05483118ddf53a78cba0f017a2adafd
amitshou13 <shouamit13@gmail.com>
2024-01-16 19:36:14 +0530

    arrays que-1

--- Arrays/MaxMin.java
+++ Arrays/MaxMin.java
@@ -1,29 +1,29 @@
 // Maximum and Minimum Element in an Array
+import java.util.*;
 
-public class MaxMin{
-    static Pair getMinMax(long a[], long n)  
-    {
-        long max=0;
-        long min=Long.MAX_VALUE;
-        for(long i:a){
-            max=Math.max(max, i);
-            min=Math.min(min, i);
+public class MaxMin {
+    static Pair getMinMax(long a[], long n) {
+        long max = 0;
+        long min = Long.MAX_VALUE;
+        for (long i : a) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
         }
         return new Pair(min, max);
     }
+
     public static void main(String[] args) {
-        long arr[]={1,9,3,5,0,11,47};
-        Pair p=getMinMax(arr, arr.length);
-        System.out.print(p.first+" "+p.second);
+        long arr[] = { 1, 9, 3, 5, 0, 11, 47 };
+        Pair p = getMinMax(arr, arr.length);
+        System.out.print(p.first + " " + p.second);
     }
 }
 
-class Pair  
-{  
-    long first, second;  
-    public Pair(long first, long second)  
-    {  
-        this.first = first;  
-        this.second = second;  
-    }  
-} 
+class Pair {
+    long first, second;
+
+    public Pair(long first, long second) {
+        this.first = first;
+        this.second = second;
+    }
+}
